1d7580736038c3c87664e4ef1bc3b386e63ed94c
beginners-sameer <49206176+beginners-sameer@users.noreply.github.com>
2023-02-27 20:15:33 +0530

    Update App.java

--- src/main/java/com/scmgalaxy/mavensample/App.java
+++ src/main/java/com/scmgalaxy/mavensample/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World - fix for login issue codes" );
+        System.out.println( "Hello super world - fix for login issue codes" );
     }
 }
